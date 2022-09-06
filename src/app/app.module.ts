3e2cf7bf15f78cbe907c01cf5cdad32a6cee0738
minhdat2708 <minhdat01.dev@gmail.com>
2022-09-06 18:24:21 +0700

    create fruit service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {FruitsModule} from "./fruits/fruits.module";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FruitsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
